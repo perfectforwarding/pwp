df3b8320e2b78fa2b3067e138edbb357c58a325c
smokingdlx <alfonsocomin@gmail.com>
2015-04-20 01:00:43 +0200

    Raymarch demo updated

--- public/balls2/balls.fs
+++ public/balls2/balls.fs
@@ -1,17 +1,18 @@
 
-precision lowp float;
+precision highp float;
 
 uniform vec3 uResolution;
 uniform vec3 uLightDir;
 uniform vec3 uCamEye;
 uniform vec3 uCamFowward;
 uniform vec3 uCamRight;
+uniform vec3 uCamUp;
 uniform float uVFov;
 
 const float PI = 3.14159265358979323846264;
-const float ZFAR = 25.0;
+const float ZFAR = 1025.0;
 const float FOCAL_LENGTH = 1.0;
-const int STEPS = 64;
+const int STEPS = 256;
 const float EPSILON = 0.001;
 
 const vec3 CAM_UP = vec3(0.0, 1.0, 0.0);
@@ -22,95 +23,112 @@ const float BALL_RADIUS = 1.0;
 const vec3 BOX_CENTER = vec3(-5.0, 0.0, 5.0);
 const vec3 BOX_HALF_EXTENTS = vec3(1.0, 1.0, 1.0);
 
+const vec4 PLANE_F = vec4(0.0, 1.0, 0.0, -1.0);
+
 float udSphere(vec3 p, vec3 sphere_center, float sphere_radius)
 {
 	return length(p - sphere_center) - sphere_radius;
 }
-
-float iSphere( in vec3 ro, in vec3 rd, in vec4 sph )
-{
-	vec3 oc = ro - sph.xyz;
-	float b = dot( oc, rd );
-	float c = dot( oc, oc ) - sph.w*sph.w;
-	float h = b*b - c;
-	if( h<0.0 ) return -1.0;
-	return -b - sqrt( h );
-}
-
 
 float sdBox(vec3 p, vec3 box_center, vec3 box_half_extents)
 {
 	return length(max(abs(p - box_center) - box_half_extents, 0.0));	
 }
 
-float distScene(vec3 p)
+float sdPlane(vec3 p, vec4 plane)
 {
-	return udSphere(p, BALL_CENTER, BALL_RADIUS);
-/*
-	return min(
-		udSphere(p, BALL_CENTER, BALL_RADIUS),
-		sdBox(p, BOX_CENTER, BOX_HALF_EXTENTS)
-	);
-*/
+	return dot(p, plane.xyz) - plane.w;
 }
-/*
+
+vec2 selElem(vec2 p1, vec2 p2)
+{
+	return p1.x < p2.x ? p1 : p2;
+}
+
+vec2 distScene(vec3 p)
+{
+	vec2 d = vec2(sdPlane(p, PLANE_F), 0.0);
+	d = selElem(vec2(sdBox(p, BOX_CENTER, BOX_HALF_EXTENTS), 1.0), d);
+	d = selElem(vec2(udSphere(p, BALL_CENTER, BALL_RADIUS), 2.0), d);
+
+	return d;
+}
+
 vec3 getNormal(vec3 p)
 {
 	float h = 0.0001;
 	return normalize(vec3(
-		distScene(p + vec3(h, 0, 0)) - distScene(p - vec3(h, 0, 0)),
-		distScene(p + vec3(0, h, 0)) - distScene(p - vec3(0, h, 0)),
-		distScene(p + vec3(0, 0, h)) - distScene(p - vec3(0, 0, h))));
+		distScene(p + vec3(h, 0, 0)).x - distScene(p - vec3(h, 0, 0)).x,
+		distScene(p + vec3(0, h, 0)).x - distScene(p - vec3(0, h, 0)).x,
+		distScene(p + vec3(0, 0, h)).x - distScene(p - vec3(0, 0, h)).x));
 }
-*/
-void rayMarch(vec3 ro, vec3 rd, out int i, out float t)
+
+
+void rayMarch(vec3 ro, vec3 rd, out int i, out float t, out float m)
 {
 	t = 0.0;
+	m = -1.0;
 	for(int j = 0; j < STEPS; ++j)
 	{
 		vec3 p = ro + rd * t;
-		//float d = distScene(p);
-		float d = iSphere(ro, rd, vec4(BALL_CENTER, BALL_RADIUS));
-		if(d < EPSILON || d > ZFAR)
+		vec2 d = distScene(p);
+		t += d.x;
+		//float d = udSphere(ro, rd, BALL_CENTER, BALL_RADIUS);
+		if(d.x < EPSILON)
+		{
+			i = j;
+			m = d.y;
+			break;
+		}
+		else if(d.x > ZFAR)
 		{
 			i = j;
 			break;
-		}
-
-		t += d;
+		}		
 	}
+}
+
+float computeShadow(vec3 p, vec3 lightDir)
+{
+	vec3 ro = p - lightDir * 100.0;
+	float t, m;
+	int i;
+	rayMarch(ro, lightDir, i, t, m);
+	return t < (100.0 - 0.01) ? 0.0 : 1.0;
 }
 
 vec3 computeColor(vec3 ro, vec3 rd)
 {
 	vec3 color = vec3(0.1, 0.1, 0.2);
 
-	float t;
+	float t, m;
 	int i;	
-	rayMarch(ro, rd, i, t);
+	rayMarch(ro, rd, i, t, m);
 
-	if(t < ZFAR)
+	if(m >= 0.0)
 	{
+		// surface point
 		vec3 p = ro + rd * t;
+		// shadow
+		float s = computeShadow(p, uLightDir);
 
 		// Calc mat color
-		//color = floor(mod(p.xyz, 2.0));
-		//float cy = floor(mod(p.y * 2.0, 3.0)) - 1.0;		
-		//float cx = floor(mod(p.x * 2.0, 3.0)) - 1.0;		
-
 		float cx = sign(mod(p.x * 2.0, 2.0) - 1.0);
 		float cy = sign(mod(p.y * 2.0, 2.0) - 1.0);
+		float cz = sign(mod(p.z * 2.0, 2.0) - 1.0);
+		color = vec3(cy * cx * cz);
 
-		//color = vec3(((cy * cx) + 1.0) / 2.0);
-		color = vec3(cy * cx);
+		color = vec3(0.5, 0.2, cos(m)) * (sin(m * 0.5) + 0.3);
 
 
-		// Calc lighting
-		//vec3 normal = getNormal(p);
-		//vec3 H = normalize(uLightDir + rd);
+		// Lit
+		vec3 normal = getNormal(p); // surface normal
+		vec3 H = normalize(uLightDir + rd); // half vector
 
-		//color *= vec3(clamp(-dot(normal, uLightDir), 0.0, 1.0));
-		//color += pow(clamp(-dot(normal, H), 0.0, 1.0), 85.0);
+		color *= vec3(max(-dot(normal, uLightDir), 0.0)); // diffuse
+		color += pow(clamp(-dot(normal, H), 0.0, 1.0), 100.0) * 2.0;
+
+		color *= vec3(s);
 
 	}
 
@@ -124,22 +142,16 @@ void main(void)
 	uv.x = gl_FragCoord.x * 2.0 / uResolution.x - 1.0;
 	uv.y = gl_FragCoord.y * 2.0 / uResolution.y - 1.0;
 
-	vec3 ro = uCamEye;
-
-	//float fl = 1.0 / tan( 45.0 / PI / 2.0);
-	//fl = FOCAL_LENGTH;
+	// focal length
 	float fl = 1.0 / tan(uVFov / 180.0 * PI / 2.0); // Distance between the eye and the image plane
 
-	fl = 1.0;
+	// ray
+	vec3 ro = uCamEye;
+	vec3 rd = normalize(uCamFowward * fl + uCamRight * uv.x * aspectRatio + uCamUp * uv.y);
 
-	//uv.x = 0.0;
-	//uv.y = 0.0;
-
-	vec3 rd = normalize(uCamFowward * fl + uCamRight * uv.x * aspectRatio + CAM_UP * uv.y);
-
+	// calc color
 	vec3 color = computeColor(ro, rd);
 
-	gl_FragColor = vec4(color, 1.0);
-
-	//gl_FragColor = vec4(rd.x, 0.0, 0.0, 1.0);
+	// output color
+	gl_FragColor = vec4(color, 1.0);	
 }
