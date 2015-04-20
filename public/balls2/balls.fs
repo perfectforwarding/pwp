9e8754580fb38fd6b7555ca1fbb97be6f078f494
smokingdlx <alfonsocomin@gmail.com>
2015-04-20 02:25:35 +0200

    soft shadow in progress

--- public/balls2/balls.fs
+++ public/balls2/balls.fs
@@ -11,9 +11,10 @@ uniform float uVFov;
 
 const float PI = 3.14159265358979323846264;
 const float ZFAR = 1025.0;
+const float SFAR = 300.0;
 const float FOCAL_LENGTH = 1.0;
 const int STEPS = 256;
-const float EPSILON = 0.001;
+const float EPSILON = 0.0001;
 
 const vec3 CAM_UP = vec3(0.0, 1.0, 0.0);
 
@@ -45,7 +46,7 @@ vec2 selElem(vec2 p1, vec2 p2)
 	return p1.x < p2.x ? p1 : p2;
 }
 
-vec2 distScene(vec3 p)
+vec2 map(vec3 p)
 {
 	vec2 d = vec2(sdPlane(p, PLANE_F), 0.0);
 	d = selElem(vec2(sdBox(p, BOX_CENTER, BOX_HALF_EXTENTS), 1.0), d);
@@ -58,20 +59,27 @@ vec3 getNormal(vec3 p)
 {
 	float h = 0.0001;
 	return normalize(vec3(
-		distScene(p + vec3(h, 0, 0)).x - distScene(p - vec3(h, 0, 0)).x,
-		distScene(p + vec3(0, h, 0)).x - distScene(p - vec3(0, h, 0)).x,
-		distScene(p + vec3(0, 0, h)).x - distScene(p - vec3(0, 0, h)).x));
+		map(p + vec3(h, 0, 0)).x - map(p - vec3(h, 0, 0)).x,
+		map(p + vec3(0, h, 0)).x - map(p - vec3(0, h, 0)).x,
+		map(p + vec3(0, 0, h)).x - map(p - vec3(0, 0, h)).x));
 }
 
+// cast a ray
+// @param ro Right origin
+// @param rd Right direction
+// @out i number of iterations
+// @out t distance from ro in rd direction to the mapped point
+// @out m material idx 
 
-void rayMarch(vec3 ro, vec3 rd, out int i, out float t, out float m)
+void march(vec3 ro, vec3 rd, float maxt, out int i, out float t, out float m)
 {
 	t = 0.0;
 	m = -1.0;
+
 	for(int j = 0; j < STEPS; ++j)
 	{
 		vec3 p = ro + rd * t;
-		vec2 d = distScene(p);
+		vec2 d = map(p);
 		t += d.x;
 		//float d = udSphere(ro, rd, BALL_CENTER, BALL_RADIUS);
 		if(d.x < EPSILON)
@@ -80,7 +88,7 @@ void rayMarch(vec3 ro, vec3 rd, out int i, out float t, out float m)
 			m = d.y;
 			break;
 		}
-		else if(d.x > ZFAR)
+		else if(d.x > maxt)
 		{
 			i = j;
 			break;
@@ -88,13 +96,32 @@ void rayMarch(vec3 ro, vec3 rd, out int i, out float t, out float m)
 	}
 }
 
-float computeShadow(vec3 p, vec3 lightDir)
+float shadow(vec3 p, vec3 lightDir)
 {
-	vec3 ro = p - lightDir * 100.0;
+	vec3 ro = p - lightDir * SFAR;
+/*		
 	float t, m;
 	int i;
-	rayMarch(ro, lightDir, i, t, m);
+	march(ro, lightDir, 100.0, i, t, m);
 	return t < (100.0 - 0.01) ? 0.0 : 1.0;
+*/	
+
+	float sf = 1.0;
+	float t = 0.0;
+	for(int i = 0; i < STEPS; ++i )
+	{
+		vec3 p = ro + lightDir * t;
+		float h = map(p).x;
+		t += h;
+		//sf = min(sf, 8.0 * h / t);		
+		sf = min(sf, (SFAR - t) / SFAR);		
+		if(h < EPSILON || t >= SFAR)
+		{
+			break;
+		}
+	}
+
+	return clamp(sf, 0.0, 1.0);
 }
 
 vec3 computeColor(vec3 ro, vec3 rd)
@@ -103,14 +130,14 @@ vec3 computeColor(vec3 ro, vec3 rd)
 
 	float t, m;
 	int i;	
-	rayMarch(ro, rd, i, t, m);
+	march(ro, rd, ZFAR, i, t, m);
 
 	if(m >= 0.0)
 	{
 		// surface point
 		vec3 p = ro + rd * t;
 		// shadow
-		float s = computeShadow(p, uLightDir);
+		float s = shadow(p, uLightDir);
 
 		// Calc mat color
 		float cx = sign(mod(p.x * 2.0, 2.0) - 1.0);
